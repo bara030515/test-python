a588dca467e7aaa0b702f569e230422259b23c41
Seungha.K <shkimm03@naver.com>
2025-03-17 11:52:56 +0900

    2주차 인풋 실습

---
+++ variable.py
@@ -0,0 +1,20 @@
+num1 = 100
+num2 = 200
+
+numSum = num1 + num2
+print(num1,'+',num2,'=',numSum)
+
+numSub = num1 - num2
+print(num1,'-',num2,'=',numSub)
+
+numMul = num1 * num2
+print(num1,'*',num2,'=',numMul)
+
+numDeb = num1 / num2
+print(num1,'/',num2,'=',numDeb)
+
+str1 = 'Hello'
+str2 = 'World'
+print(str1)
+
+res = 
