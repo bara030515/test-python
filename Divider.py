88341e980658aed3ee6420fda32036fb88648b47
se <dfasdfasdf>
2025-03-31 10:57:54 +0900

    4주차 실습 1

--- Divider.py
+++ Divider.py
@@ -1,20 +1,34 @@
 import tkinter
 
+# 버튼 클릭하면 실행하는 함수
 def btn_click():
+    # 값 입력(글자여서 숫자로 바꿔야 함)
     num1 = int(entry1.get())
     num2 = int(entry2.get())
 
+    # 라벨에 넣은 문자
     str1 = str(num1) + '을(를)' + str(num2) + '로 나눈 몫은' + str(num1//num2) + '입니다.'
     str2 = str(num1) + '을(를)' + str(num2) + '로 나눈 나머지는' + str(num1%num2) + '입니다.'
 
+    # 라벨
     labelRes1 = tkinter.Label(root,text=str1, font=('맑은고딕', 10))
     labelRes2 = tkinter.Label(root,text=str2, font=('맑은고딕', 10))
     labelRes1.place(x=21, y=86)
     labelRes2.place(x=21, y=124)
 
+def mouseMove(event):
+    x = event.x
+    y = event.y
+    labelMouse.config(text=str(x)+','+str(y))
+    labelMouse.place(x=0,y=200)
+
 root = tkinter.Tk()
 root.title('산술연산자')
 root.geometry('400x300')
+
+root.bind('<Motion>',mouseMove)
+labelMouse = tkinter.Label(root, text=',', font=("맑은 고딕", 10))
+
 
 label1 = tkinter.Label(root,text='나눠지는 수', font=('맑은 고딕', 10))
 label2 = tkinter.Label(root,text='나누는 수', font=('맑은 고딕', 10))
