fa13a81c324455cdfe2db08631bab8968f291df6
se <dfasdfasdf>
2025-04-07 11:36:41 +0900

    숫자비교 오류 없음

--- ifTest.py
+++ ifTest.py
@@ -3,7 +3,7 @@ num = int(input('숫자 입력=>'))
 if num < 1000:
     if 100 >= num:
         print("100보다 작음")
-    else 100 <= num < 1000:
+    else:
         print('100이상 1000미만')
 else :
         print ("1000이상")
